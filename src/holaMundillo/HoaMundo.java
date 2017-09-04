d338a28f414545dc43de47258d280a1cb0e01b6c
JamDiaz <jamdiaz@outlook.com>
2017-09-04 12:29:17 +0200

    Este es un nuevo saludo

--- src/holaMundillo/HoaMundo.java
+++ src/holaMundillo/HoaMundo.java
@@ -5,7 +5,7 @@ public class HoaMundo {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		System.out.println("Hola Compi");
+		System.out.println("Hola y gracias por invitarme");
 
 	}
 
